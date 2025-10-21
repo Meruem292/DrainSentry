3f255bb4cc53fc20a781f2b455c27e0ed1b52a4a
secret arcane <newroskoto@gmail.com>
2025-10-21 08:43:19 +0000

    Maybe Better to make it TEXT and just style it with the same color.

--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -1,28 +1,11 @@
-import * as React from "react"
+import * as React from "react";
+import { cn } from "@/lib/utils";
 
-const Logo = (props: React.SVGProps<SVGSVGElement>) => (
-  <svg viewBox="0 0 120 28" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
-    <text
-      x="0"
-      y="22"
-      fontFamily="'PT Sans', sans-serif"
-      fontSize="24"
-      fontWeight="bold"
-      fill="hsl(var(--primary))"
-    >
-      Drain
-    </text>
-    <text
-      x="58"
-      y="22"
-      fontFamily="'PT Sans', sans-serif"
-      fontSize="24"
-      fontWeight="bold"
-      fill="hsl(var(--accent))"
-    >
-      Sentry
-    </text>
-  </svg>
-)
+const Logo = ({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) => (
+  <div className={cn("font-bold text-2xl font-headline", className)} {...props}>
+    <span style={{ color: 'hsl(var(--primary))' }}>Drain</span>
+    <span style={{ color: 'hsl(var(--accent))' }}>Sentry</span>
+  </div>
+);
 
 export default Logo;
