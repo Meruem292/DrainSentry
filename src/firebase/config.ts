53599e8fa06d64285a772f33562f972bc48a24aa
secret arcane <newroskoto@gmail.com>
2025-10-21 05:27:41 +0000

    its hard coded in config.ts  , it easily accessible, when i push this co

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,12 +1,12 @@
 // It's safe to expose this configuration to the client
 // See: https://firebase.google.com/docs/projects/learn-more#config-files-objects
 export const firebaseConfig = {
-  apiKey: "AIzaSyD50Lsmg3khTmYkGiu7LREqivXsBkePQMI",
-  authDomain: "drainsentry.firebaseapp.com",
-  databaseURL: "https://drainsentry-default-rtdb.firebaseio.com",
-  projectId: "drainsentry",
-  storageBucket: "drainsentry.firebasestorage.app",
-  messagingSenderId: "610406293973",
-  appId: "1:610406293973:web:e112664f4dbfd9d6dd1d5c",
-  measurementId: "G-PF2451RX9Q"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
