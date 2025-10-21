00d7876da104e5ef74ea0d71af7aeba88fbe49c7
secret arcane <newroskoto@gmail.com>
2025-10-21 05:22:56 +0000

    just do what i'm telling you.
    this is my firebase config
    
    const fire

---
+++ src/firebase/config.ts
@@ -0,0 +1,12 @@
+// It's safe to expose this configuration to the client
+// See: https://firebase.google.com/docs/projects/learn-more#config-files-objects
+export const firebaseConfig = {
+  apiKey: "AIzaSyD50Lsmg3khTmYkGiu7LREqivXsBkePQMI",
+  authDomain: "drainsentry.firebaseapp.com",
+  databaseURL: "https://drainsentry-default-rtdb.firebaseio.com",
+  projectId: "drainsentry",
+  storageBucket: "drainsentry.firebasestorage.app",
+  messagingSenderId: "610406293973",
+  appId: "1:610406293973:web:e112664f4dbfd9d6dd1d5c",
+  measurementId: "G-PF2451RX9Q"
+};
