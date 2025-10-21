3ec968ec32285775cb0c9b73c2accfbc28875526
secret arcane <newroskoto@gmail.com>
2025-10-21 14:13:36 +0000

    i already put my gemini API key, in both .env and .env.local
    GEMINI_A

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,6 +1,6 @@
 
 import {genkit} from 'genkit';
-import {googleAI} from '@genkit-ai/googleai';
+import {googleAI} from '@genkit-ai/google-genai';
 
 if (typeof process.env.NEXT_PUBLIC_GEMINI_API_KEY === 'string') {
   process.env.GEMINI_API_KEY = process.env.NEXT_PUBLIC_GEMINI_API_KEY;
@@ -8,5 +8,4 @@ if (typeof process.env.NEXT_PUBLIC_GEMINI_API_KEY === 'string') {
 
 export const ai = genkit({
   plugins: [googleAI()],
-  model: 'googleai/gemini-2.0-flash',
 });
